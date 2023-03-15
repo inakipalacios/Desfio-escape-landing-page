b9fba0d0041ebc46036822d5db0b0ae6449b996c
lgarmendiadi20dw <lgarmendiadi20dwiikzubirimanteo.com>
2023-03-15 17:17:03 +0100

    cambion nav

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -3,36 +3,43 @@ import React from "react";
 function Menu() {
     return (
         <div className="row">
-           <nav class=" navbar navbar-expand-lg bg-body-tertiary navbar  navegador">
-
-            <div class="row col-12">
-            <div class="col-7 col-lg-9 color2">
-                <div class="nav-item ">
-                <a class="navbar-brand" href=" ">
-                    <img src={require("../img/logo.png")} class="logo" alt="..."/></a>
-                    
-                        </div>
-                    
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src={require("../img/logo.png")} width="140"/></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link  boton" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link boton" href="#">Link</a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a class="nav-link boton " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu ">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr class="dropdown-divider" /></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled">Disabled</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-danger" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
-
-                <div class=" navbar-collapse d-flex col-4 col-lg-3 color3" >
-                    
-
-                <button class="btn botonRojo" type="submit">
-                            Iniciar sesion
-                        </button>
-            <button class="btn botonRojo" type="submit">
-                            Registrarse
-                        </button>
-
-                </div>
-            </div>
-            
-            
-                
             </nav>
         </div>
-        
     );
 }
 
